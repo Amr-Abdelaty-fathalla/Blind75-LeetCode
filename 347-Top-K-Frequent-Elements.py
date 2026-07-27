bdaf003828e7d14728e03843bf5a8e6bb1d68daf
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2026-07-27 23:12:46 +0300

    [347] [Time Beats: 52.21%] [Memory Beats: 99.21%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -1,24 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
         # First Sol: O(n)
+        cnt_dct = {}
+
         if len(nums) == 0:
             return []
 
-        top_k_dct = {}
-
         for num in nums:
-            if num not in top_k_dct:
-                top_k_dct[num] = 1
+            if num not in cnt_dct:
+                cnt_dct[num] = 1
             
             else:
-                top_k_dct[num] += 1
+                cnt_dct[num] += 1
         
-        top_k_srt= sorted(top_k_dct.items(), key= lambda item: item[1], reverse= True)
-        top_k_srt = top_k_srt[:k]
-        
-
-        return [item[0] for item in top_k_srt]
+        srt_cnt_dct = sorted(cnt_dct.items(), key = lambda item: item[1], reverse= True)
+        return [item[0] for item in srt_cnt_dct[:k]]
 
         
         
