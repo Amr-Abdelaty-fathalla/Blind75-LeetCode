04f8e59be625dab4e706137f4a22936b7980d918
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2026-07-31 16:04:51 +0300

    [238] [Time Beats: 93.03%] [Memory Beats: 94.12%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -1,20 +1,37 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Sol O(n)
-        n = len(nums)
-        ans = [1] * n
+        # First sol time:O(n), space: O(n)
+        # prefix_pro = []
+        # suffix_pro = []
+        # ans= []
+
+        # l = 1
+        # for i in range(len(nums)):
+        #     prefix_pro.append(l)
+        #     l *= nums[i]
+        
+        # r = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     suffix_pro.append(r)
+        #     r *= nums[i]
+
+        # for j in range(len(prefix_pro)):
+        #     ans.append(prefix_pro[j] * suffix_pro[(len(suffix_pro)-1) - j])
+
+        # return ans 
+
+        # Second Sol time: O(n), space: O(1)
+        ans = [1] * len(nums)
 
         l = 1
-        for i in range(n):
+        for i in range(len(nums)):
             ans[i] = l
             l *= nums[i]
-        
+
         r = 1
-        for i in range(n-1,-1,-1):
+        for i in range(len(nums) - 1, -1, -1):
             ans[i] *= r
             r *= nums[i]
         
         return ans
-
-        
         
