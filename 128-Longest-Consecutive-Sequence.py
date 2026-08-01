de66376bc43a631916042a948ada9de658673d76
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2026-08-01 04:32:13 +0300

    [128] [Time Beats: 99.58%] [Memory Beats: 66.24%] - LeetPush

--- 128-Longest-Consecutive-Sequence.py
+++ 128-Longest-Consecutive-Sequence.py
@@ -1,28 +1,43 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        long_seq = []
-        cnt = 1
+        # long_seq = []
+        # cnt = 1
 
-        if len(nums) == 0:
-            return 0
+        # if len(nums) == 0:
+        #     return 0
         
-        if len(nums) == 1:
-            return 1
+        # if len(nums) == 1:
+        #     return 1
         
-        nums.sort()
+        # nums.sort()
      
-        for idx in range(1, len(nums)):
-            if nums[idx] - 1 == nums[idx-1]:
-                cnt += 1
-            elif nums[idx] == nums[idx-1]:
-                cnt = cnt
-            else:
-                long_seq.append(cnt)
-                cnt = 1
+        # for idx in range(1, len(nums)):
+        #     if nums[idx] - 1 == nums[idx-1]:
+        #         cnt += 1
+        #     elif nums[idx] == nums[idx-1]:
+        #         cnt = cnt
+        #     else:
+        #         long_seq.append(cnt)
+        #         cnt = 1
         
-        if (nums[-1] == nums[-2]) or (nums[-1] - 1 == nums[-2]):
-            long_seq.append(cnt)    
-        return max(long_seq)
+        # long_seq.append(cnt)    
+        # return max(long_seq)
+
+
+        # Second Sol time: O(n)
+        hashset = set(nums)
+        maxlen = 0
+
+        for num in hashset:
+            if num - 1 not in hashset:
+                next_num = num + 1
+
+                while next_num in hashset:
+                    next_num += 1
+                
+                maxlen = max(maxlen, next_num - num)
+        
+        return maxlen
 
 
         
